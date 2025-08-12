c7bca162ecc2312511021c08a59c298a958d09a6
Erick-Mtz <er-ick258@hotmail.com>
2025-08-12 18:56:32 -0500

    feat: update model

--- src/main/java/com/products/api/repository/entity/ProductEntity.java
+++ src/main/java/com/products/api/repository/entity/ProductEntity.java
@@ -9,15 +9,21 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Table(name = "products")
 @Data
 @Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 @EqualsAndHashCode(callSuper = true)
 public class ProductEntity extends BaseEntity {
 
