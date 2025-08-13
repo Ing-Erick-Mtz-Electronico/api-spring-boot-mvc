b795c40054b1c5e0aa579ff823ef08ad871a62ed
Erick-Mtz <er-ick258@hotmail.com>
2025-08-12 21:39:09 -0500

    feat: add price field

--- src/main/java/com/products/api/dto/product/ProductToListDto.java
+++ src/main/java/com/products/api/dto/product/ProductToListDto.java
@@ -1,5 +1,7 @@
 package com.products.api.dto.product;
 
+import java.math.BigDecimal;
+
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,4 +11,5 @@ public class ProductToListDto {
     private Long id;
     private String name;
     private String description;
+    private BigDecimal price;
 }
